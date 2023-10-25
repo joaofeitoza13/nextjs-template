175083eced38d2e714905049f5ff7c59e4c37bd9
João Feitoza <joaofeitoza.13@gmail.com>
2023-10-25 01:03:23 -0300

    feat: add base layout, page and loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
+import SidebarLayout from '@/components/layouts/sidebar/SidebarLayout';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PrimaryLayout>
+          <SidebarLayout />
+          {children}
+        </PrimaryLayout>
+      </body>
     </html>
   );
 }
